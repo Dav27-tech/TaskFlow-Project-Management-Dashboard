ea97293e68f740aafff73458ee453a73fae98729
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-04 11:26:08 +0200

    Tasks page updated

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import taskList from "../data/tasks";
+import { useNavigate } from "react-router-dom";
+
 export default function Tasks() {
+  const navigate = useNavigate();
+
+  const [tasks, setTasks] = useState(taskList);
   const handleClick = () => {
-    Navigate(`/task/${taskList.id}`);
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => navigate(`/task/${task.id}`)),
+    );
   };
 
   return (
     <div>
-      <div onClick={handleClick}></div>
+      {taskList.map((task) => (
+        <div onClick={handleClick} key={task.id} style={{ cursor: "pointer" }}>
+          <h2>{task.title}</h2>
+          <p>{task.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
