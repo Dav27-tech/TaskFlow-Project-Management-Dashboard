44e69528df37c9824abcf5f4735285ce77cc44d0
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-07 16:10:10 +0200

    Searchbar component created

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,39 @@
-export default function SearchBar() {}
+import taskList from "../data/tasks";
+export default function SearchBar({
+  search,
+  setSearch,
+  statusFilter,
+  setStatusFilter,
+  priorityFilter,
+  setPriorityFilter,
+}) {
+  return (
+    <div>
+      <input
+        type="text"
+        value={search}
+        placeholder="Search task ..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <select
+        value={statusFilter}
+        onChange={(e) => setStatusFilter(e.target.value)}
+      >
+        <option value="all">All Status</option>
+        <option value="completed">Completed</option>
+        <option value="pending">Pending</option>
+      </select>
+
+      <select
+        value={priorityFilter}
+        onChange={(e) => setPriorityFilter(e.target.value)}
+      >
+        <option value="all">All Priorities</option>
+        <option value="high">High</option>
+        <option value="medium">Medium</option>
+        <option value="low">Low</option>
+      </select>
+    </div>
+  );
+}
