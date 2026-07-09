25b6f06cd02b8e968a48b50368a902a7037b88e3
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-07-09 11:31:20 +0300

    refactor: move BrowserRouter out of AppRoute

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,16 +1,22 @@
-// Import React router
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-// Import pages
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
+import Projects from "../pages/Projects";
+import ProjectDetails from "../pages/ProjectDetails";
 import Tasks from "../pages/Tasks";
 import TaskDetails from "../pages/TaskDetails";
+import Team from "../pages/Team";
+import About from "../pages/About";
 
 export default function AppRoute() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:id" element={<ProjectDetails />} />
       <Route path="/tasks" element={<Tasks />} />
-      <Route path="/task/:id" element={<TaskDetails />} />
+      <Route path="/tasks/:id" element={<TaskDetails />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
