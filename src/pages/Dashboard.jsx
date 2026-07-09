834b2a474a5da31c904e823811c81e74baf5b180
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-09 20:18:04 +0200

    Remove Navbar component from Dashboard and simplify layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,10 @@
 // Import main components
-import Navbar from "../components/Navbar";
 import StatisticCard from "../components/StatisticCard";
 
 export default function Dashboard() {
   return (
     <div>
-      <Navbar />
-      <main>
-        <StatisticCard />
-      </main>
+      <StatisticCard />
     </div>
   );
 }
