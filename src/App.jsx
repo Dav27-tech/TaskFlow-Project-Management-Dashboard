1e904d0751dcf394652e32f6fb2bd01b6fdab23a
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-07-10 19:07:41 +0300

    Wrap Navbar and AppRoute components with TaskProvider in App component; remove unused Users import in StatisticCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AppRoute from "./routes/AppRoute";
+import { TaskProvider } from "./components/Model";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <AppRoute />
+      <TaskProvider>
+        <Navbar />
+        <AppRoute />
+      </TaskProvider>
     </BrowserRouter>
   );
 }
